a84b4fc2da1c694861e5a350e0815b7846498a89
Rohan Mahanta <rohanmahanta2005@gmail.com>
2026-08-11 19:31:25 +0530

    Added

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    var name: String = "Rohan"
+    var age: Int = 10
+    var marks: Float = 10F
+    var isStudent: Boolean = true
+
+    val num1 = 10
+    val num2 = 30
+
+    println("num1+num2 = ${num1+num2}")
+    println("num1-num2 = ${num1-num2}")
+    println("num1*num2 = ${num1*num2}")
+    println("num1/num2 = ${num1/num2}")
+
+}
